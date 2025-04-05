df47b37521460c75531bf871c9aca4c543de05a0
Zac Grey <zacgrey2005@icloud.com>
2025-04-05 17:53:59 +0100

    Text above cat follows player

--- Assets/Scripts/DictationScript.cs
+++ Assets/Scripts/DictationScript.cs
@@ -1,21 +1,26 @@
 using Oculus.Voice.Dictation;
+using TMPro;
 using UnityEngine;
 
 public class DictationScript : MonoBehaviour
 {
     AppDictationExperience appDictationExperience;
+    public GameObject TextObject;
 
     void Awake()
     {
+        TextMeshProUGUI textbox = TextObject.GetComponent<TextMeshProUGUI>();
         appDictationExperience = GetComponent<AppDictationExperience>();
         appDictationExperience.DictationEvents.OnPartialTranscription.AddListener((transcription) =>
         {
             Debug.Log(transcription);
+            textbox.text = transcription;
         });
 
         appDictationExperience.DictationEvents.OnFullTranscription.AddListener((transcription) =>
         {
             Debug.Log(transcription);
+            textbox.text = transcription;
         });
 
     }
